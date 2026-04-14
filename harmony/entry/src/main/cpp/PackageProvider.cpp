ae960cbd5d3b21e0ae87f1eb73c2da45bcb0ebe1
xiezhiyan <16297996+xiezhiyan@users.noreply.github.com>
2026-04-14 12:02:42 +0800

    范例依赖添加

--- harmony/entry/src/main/cpp/PackageProvider.cpp
+++ harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -2,10 +2,16 @@
 #include "generated/RNOHGeneratedPackage.h"
 #include "WebMapPackage.h"
 #include "WebViewPackage.h"
+#include "RNFSPackage.h"
+#include "SafeAreaViewPackage.h"
+
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
     return {std::make_shared<RNOHGeneratedPackage>(ctx),
             std::make_shared<WebMapPackage>(ctx),
-            std::make_shared<WebViewPackage>(ctx)};
+            std::make_shared<WebViewPackage>(ctx),
+            std::make_shared<SafeAreaViewPackage>(ctx),
+            std::make_shared<RNFSPackage>(ctx)
+    };
 }
